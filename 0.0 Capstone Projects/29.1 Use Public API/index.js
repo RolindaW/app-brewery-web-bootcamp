26023716a9b3d5753c1b4252bba96ed25691dc66
RolindaW <56414667+RolindaW@users.noreply.github.com>
2024-07-04 15:08:38 +0900

    Use Public API request item list

--- 0.0 Capstone Projects/29.1 Use Public API/index.js
+++ 0.0 Capstone Projects/29.1 Use Public API/index.js
@@ -1,11 +1,13 @@
 import express from "express";
+import axios from "axios";
 
 const app = express();
 const port = 3000;
+const API_BASE_URL = "https://eldenring.fanapis.com/api/";
 
 let featureName = undefined;
 let page = undefined;
-let items = undefined;
+const limit = 50;
 
 // Define - by registering as a middleware - root directory of static resources (those referenced by template documents e.g. assets or styles)
 app.use(express.static("public"));
@@ -21,22 +23,35 @@ app.get("/", (req, res) => {
     res.render("index.ejs", { features: getFeatures() });
 });
 
-app.get("/armor", (req, res) => {
+app.get("/armor", async (req, res) => {
     // Reset page index if corresponding feature card page is not being redirected by itself (i.e. paging)
     if (featureName != "armor") {
         featureName = "armor";
         page = 1;
     }
 
-    items = getItemsMock();
-    // items = getItems();
+    // Get corresponding items
+    try {
+        const result = await axios.get(getRoute(), getParameters());
+        const data = result.data;
+        // console.log(data);
 
-    res.render("feature-card.ejs", {
-        features: getFeatures(),
-        featureName: featureName,
-        page: page,
-        items: items,
-    });
+        const items = formatItems(data.data);
+        // console.log(items);
+
+        res.render("feature-card.ejs", {
+            features: getFeatures(),
+            featureName: featureName,
+            page: page,
+            limit: limit,
+            items: items,
+        });
+    } catch (error) {
+        res.render("feature-card.ejs", {
+            features: getFeatures(),
+            error: JSON.stringify(error.response.data),
+        });
+    }
 });
 
 app.get("/armor-prev", (req, res) => {
@@ -59,8 +74,33 @@ function getFeatures() {
     return [new Feature("Armor", "/armor", new Image("assets/images/armor.svg", "armor")), new Feature("Weapon", "/weapon", new Image("assets/images/weapon.svg", "weapon")), new Feature("Ammo", "/ammo", new Image("assets/images/ammo.svg", "ammo"))];
 }
 
-function getItemsMock() {
-    return [new Item("Item 1", new Image("assets/images/armor.svg", "sample item 1")), new Item("Item 2", new Image("assets/images/armor.svg", "sample item 2")), new Item("Item 3", new Image("assets/images/armor.svg", "sample item 3"))];
+function formatItems(rawItems) {
+    let formattedItems = [];
+
+    rawItems.forEach((rawItem) => {
+        const formattedItem = new Item(rawItem.id, rawItem.name, new Image(rawItem.image, rawItem.description));
+        formattedItems.push(formattedItem);
+    });
+
+    return formattedItems;
+}
+
+function getRoute() {
+    switch (featureName) {
+        case "armor":
+            return API_BASE_URL + "armors";
+        default:
+            return API_BASE_URL;
+    }
+}
+
+function getParameters() {
+    return {
+        params: {
+            limit: limit,
+            page: page - 1,
+        },
+    };
 }
 
 class Feature {
@@ -72,7 +112,8 @@ class Feature {
 }
 
 class Item {
-    constructor(name, image) {
+    constructor(id, name, image) {
+        this.id = id;
         this.name = name;
         this.image = image;
     }
